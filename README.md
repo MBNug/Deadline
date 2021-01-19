00386f1d05aaf81948ac94e50ae0dfbed20f5659
mrizkitriyanto <muhamadrizkitriyanto41@gmail.com>
2021-01-19 22:03:35 +0700

    initial commit tapi udah kelar

---
+++ README.md
@@ -0,0 +1,7 @@
+# Countdown
+Web untuk ngitung deadline
+
+#UNTUK SALITI TERCINTA
+yang perlu diubah saat update deadline adalah
+    1. pada HTML yang diubah Judul(h2)-nya aja
+    2. js yang diubah TANGGAL TUJUAN-nya
---
+++ assets/js/diklat.js
@@ -0,0 +1,42 @@
+
+// Waktu yang di tuju
+var countDate = new Date('Jan 22, 2021 00:00:00').getTime(); //TANGGAL TUJUAN
+
+// Fungsi countdown
+function deadline(){
+    var now = new Date().getTime(); //SEKARANG
+        gap = countDate - now; //Selisih
+
+        var second = 1000;
+        var minute = second * 60;
+        var hour = minute * 60;
+        var day = hour * 24;
+
+        var d = Math.floor(gap / (day));
+        var h = Math.floor((gap % (day)) / (hour));
+        var m = Math.floor((gap % (hour)) / (minute));
+        var s = Math.floor((gap % (minute)) / (second));
+
+        document.getElementById('day').innerText = d;
+        document.getElementById('hour').innerText = h;
+        document.getElementById('minute').innerText = m;
+        document.getElementById('second').innerText = s;
+
+}
+
+setInterval(function() {
+    deadline();
+}, 1000);
+
+
+/* SCROLL REVEAL ANIMATION */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+sr.reveal('.countdown__container', {origin:'right', delay: 400});
+sr.reveal('.countdown__container-subtitle', {origin:'left', delay: 500});
+sr.reveal('.countdown__timer-time', {delay: 800});
